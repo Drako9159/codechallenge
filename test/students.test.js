7a8d74b50ec9e09580c2c406a7c1ceaaa8a1b4ed
Drako9159 <chochito9159@gmail.com>
2022-04-28 19:29:49 -0500

    add 3 test passed

--- test/students.test.js
+++ test/students.test.js
@@ -8,13 +8,12 @@ describe("Tests Students", () => {
     });
     test("Test2: for getStudentsCertification", () => {
         const students = [{certificated: "check", haveCertification: true, email: "gravers9@getMaxListeners.com" }];
-        const studentsHave = StudentsService.getStudentsCertification(students, true);
-        expect(studentsHave).toEqual(["gravers9@getMaxListeners.com"]);
+        const studentsEmail = StudentsService.getStudentsCertification(students, true);
+        expect(studentsEmail).toEqual(["gravers9@getMaxListeners.com"]);
     });
     test("Test3: for getStudentsCreditsOver500", () => {
         const students = [{credits : 300},{credits: 500}, {credits: 900}];
-        const studentsHave = StudentsService.getStudentsCreditsOver500(students, 600);
-        expect(studentsHave).toEqual([{credits : 900}]);
+        const studentsOver500 = StudentsService.getStudentsCreditsOver500(students, 600);
+        expect(studentsOver500).toEqual([{credits : 900}]);
     });
-
 });
